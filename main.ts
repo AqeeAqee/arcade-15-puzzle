3c9c486cadc27ca608b51d23907d08339a985295
AqeeAqee <aqee@msn.com>
2023-09-02 02:21:51 +0800

    Shrink addWall codes; rename totalRow/Col -> Rows/Columns

--- main.ts
+++ main.ts
@@ -4,45 +4,49 @@ const GAP = 3, TILE_SIZE = 24
 const INTERVAL = GAP + TILE_SIZE
 const bg = scene.backgroundImage()
 
-namespace NumberTiles {
-    export let totalRow = 0, totalCol = 0
+namespace Board {
+    export let Rows = 0, Columns = 0
     let emptyRow = 0, emptyCol = 0
     const boardCells: Tile[][] = [];
     const wallsHrz: boolean[][] = []
     const wallsVrt: boolean[][] = []
     const offsetDir = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-    export function initTiles() {
-        const BOARD_WIDTH = INTERVAL * totalCol + GAP
-        const BOARD_HEIGHT = INTERVAL * totalRow + GAP
+    export function initBoard() {
+        const BOARD_WIDTH = INTERVAL * Columns + GAP
+        const BOARD_HEIGHT = INTERVAL * Rows + GAP
         Board_Left = (screen.width - BOARD_WIDTH) >> 1
         Board_Top = (screen.height - BOARD_HEIGHT) >> 1
 
         bg.fill(13)
         bg.fillRect(Board_Left, Board_Top, BOARD_WIDTH, BOARD_HEIGHT, 12)
-        for (let i = 0, n = 1; i < totalRow; i++) {
+        for (let i = 0, n = 1; i < Rows; i++) {
             boardCells.push([])
-            for (let j = 0; j < totalCol; j++) {
+            wallsHrz.push([])
+            wallsVrt.push([])
+            for (let j = 0; j < Columns; j++) {
                 bg.fillRect(Board_Left + GAP + j * INTERVAL, Board_Top + GAP + i * INTERVAL, TILE_SIZE, TILE_SIZE, 15)
-                if (n != (totalCol * totalRow))
+                if (n != (Columns * Rows))
                     boardCells[i][j] = new Tile(n++, i, j);
+                wallsHrz[i].push(false)
+                wallsVrt[i].push(false)
             }
         }
 
-        emptyRow = totalRow - 1
-        emptyCol = totalCol - 1;
+        emptyRow = Rows - 1
+        emptyCol = Columns - 1;
     }
 
     function get(row: number, col: number): Tile {
-        return (0 <= row && row < totalRow && 0 <= col && col < totalCol) ?
+        return (0 <= row && row < Rows && 0 <= col && col < Columns) ?
             boardCells[row][col] :
             null
     }
 
     function solved(): boolean {
         let c = 1;
-        for (let row = 0; row < totalRow; ++row)
-            for (let col = 0; col < totalCol; ++col, c++) {
+        for (let row = 0; row < Rows; ++row)
+            for (let col = 0; col < Columns; ++col, c++) {
                 const tile = get(row, col)
                 if (tile && tile.n !== c)
                     return false;
@@ -84,7 +88,7 @@ namespace NumberTiles {
         pause(200);
         scene.cameraShake(5, 500);
         pause(600);
-        let count = (totalCol * totalRow) ** 2 >> 2
+        let count = (Columns * Rows) ** 2 >> 2
         while (count)
             if (randomMove())
                 --count
@@ -120,8 +124,8 @@ namespace NumberTiles {
         myMenu.title = miniMenu.createMenuItem("15 Puzzle")
         myMenu.onButtonPressed(controller.A, (itemTitle, i) => {
             const dimension = itemTitle.split("x")
-            NumberTiles.totalRow = parseInt(dimension[0])
-            NumberTiles.totalCol = parseInt(dimension[1])
+            Rows = parseInt(dimension[0])
+            Columns = parseInt(dimension[1])
             myMenu.close()
             menuDone = true
         })
@@ -135,11 +139,11 @@ namespace NumberTiles {
         const targetCol = col + offsetCol
 
         if (offsetRow) {
-            if (targetRow < 0 || targetRow >= totalRow) return false
+            if (targetRow < 0 || targetRow >= Rows) return false
             if (wallsHrz[Math.min(row, targetRow)][col]) return false
         }
         if (offsetCol) {
-            if (targetCol < 0 || targetCol >= totalCol) return false
+            if (targetCol < 0 || targetCol >= Columns) return false
             if (wallsVrt[row][Math.min(col, targetCol)]) return false
         }
         return true
@@ -154,58 +158,41 @@ namespace NumberTiles {
     }
 
     export function addWalls(n: number) {
-        for (let i = 0, n = 1; i < totalRow; i++) {
-            wallsHrz.push([])
-            wallsVrt.push([])
-            for (let j = 0, n = 1; j < totalCol; j++) {
-                wallsHrz[i].push(false)
-                wallsVrt[i].push(false)
-        }}
         while (n) {
-            if (Math.percentChance(50)) { //try horizontal wall
-                const tryRow = Math.randomRange(0, totalRow - 2)
-                const tryCol = Math.randomRange(0, totalCol - 1)
-                if (!wallsHrz[tryRow][tryCol]) {
-                    wallsHrz[tryRow][tryCol] = true
-                    if (countMovableDirections(tryRow, tryCol) >= 2 && countMovableDirections(tryRow + 1, tryCol) >= 2) {
-                        bg.fillRect(Board_Left + GAP + tryCol * INTERVAL + 1, Board_Top + (tryRow + 1) * INTERVAL + 1, TILE_SIZE - 2, 1, 5)
-                        n--
-                    }
-                    else 
-                        wallsHrz[tryRow][tryCol] = false
-                }
-            }
-            else{//try vertical wall
-                const tryRow = Math.randomRange(0, totalRow - 1)
-                const tryCol = Math.randomRange(0, totalCol - 2)
-                if (!wallsVrt[tryRow][tryCol]) {
-                    wallsVrt[tryRow][tryCol] = true
-                    if (countMovableDirections(tryRow, tryCol) >= 2 && countMovableDirections(tryRow, tryCol + 1) >= 2) {
-                        bg.fillRect(Board_Left + (tryCol + 1) * INTERVAL + 1, Board_Top + GAP + tryRow  * INTERVAL + 1, 1 , TILE_SIZE - 2, 5)
-                        n--
-                    }
-                    else 
-                        wallsVrt[tryRow][tryCol] = false
-                }
+            const HV = Math.percentChance(50) // true=horizontal, false=vertical
+            const walls = HV ? wallsHrz : wallsVrt
+            const tryRow = Math.randomRange(0, Rows - (HV ? 2 : 1))
+            const tryCol = Math.randomRange(0, Columns - (HV ? 1 : 2))
+            if (!walls[tryRow][tryCol]) {
+                walls[tryRow][tryCol] = true
+                if (countMovableDirections(tryRow, tryCol) >= 2 // left/top of the wall
+                    && countMovableDirections(tryRow + (HV ? 1 : 0), tryCol + (HV ? 0 : 1)) >= 2) { // right/bottom of the wall
+                    if (HV)
+                        bg.fillRect(Board_Left + GAP + tryCol * INTERVAL + 2, Board_Top + (tryRow + 1) * INTERVAL + 1, TILE_SIZE - 4, 1, 5)
+                    else
+                        bg.fillRect(Board_Left + (tryCol + 1) * INTERVAL + 1, Board_Top + GAP + tryRow * INTERVAL + 2, 1, TILE_SIZE - 4, 5)
+                    n--
+                } else
+                    walls[tryRow][tryCol] = false
             }
         }
     }
 }
 
-NumberTiles.chooseDimension()
-NumberTiles.initTiles()
-NumberTiles.addWalls(Math.idiv(NumberTiles.totalRow + NumberTiles.totalCol - 4, 2))
-NumberTiles.shuffle()
+Board.chooseDimension()
+Board.initBoard()
+Board.addWalls(Math.idiv(Board.Rows + Board.Columns - 4, 2))
+Board.shuffle()
 
 controller.left.onEvent(ControllerButtonEvent.Pressed, () => {
-    NumberTiles.doMove(Direction.LEFT);
+    Board.doMove(Direction.LEFT);
 });
 controller.right.onEvent(ControllerButtonEvent.Pressed, () => {
-    NumberTiles.doMove(Direction.RIGHT);
+    Board.doMove(Direction.RIGHT);
 });
 controller.up.onEvent(ControllerButtonEvent.Pressed, () => {
-    NumberTiles.doMove(Direction.UP);
+    Board.doMove(Direction.UP);
 });
 controller.down.onEvent(ControllerButtonEvent.Pressed, () => {
-    NumberTiles.doMove(Direction.DOWN);
+    Board.doMove(Direction.DOWN);
 });
